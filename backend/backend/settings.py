3db7f708031f149aa81d94801d43fbf2d610e8fd
Pham Hoang <pxhoang@eccentex.com>
2024-12-26 21:30:25 +0100

    Invalidate the old secret key and generated a new one

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -27,6 +27,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.getenv('SECRET_KEY')
 
+if not SECRET_KEY:
+    raise ValueError('The SECRET_KEY setting must not be empty.')
+
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
